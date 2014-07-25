13d810d701207017fb4c1a5e7d9b049f39c50863
& <pair+jdcohen9.mgavaudan@devbootcamp.com>
2014-07-24 23:13:46 -0400

    create-user test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,17 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
+
+
+  describe "GET #create" do
   let(:user) {FactoryGirl.build :user}
+    it "saves a new user to the database" do
+      expect {post :create, :user => @user}.to change(User, :count).by(1)
+    end
+  end
 
 
-  # describe "GET #new User" do
-  #   it "renders the new user page" do
-  #     get :new
-  #     expect(response).to render_template :new
-  #   end
+# login a user who exists
+
+# deny login permission w/o valid name/username/password
+
+# deny signup permission w/o valid name/username/password
 
 
 
 
-  # end
+
 end
