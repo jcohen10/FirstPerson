ff3787b88e7bdf506458bf7a74f16d86581f86ee
Joe <jdcohen9@gmail.com>
2014-07-25 15:42:33 -0400

    user-update tests, we're not using that route atm though

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,53 +1,32 @@
 require 'rails_helper'
 # are we also going to use capybara to simulate
-# # RSpec.describe UsersController, :type => :controller do
+RSpec.describe UsersController, :type => :controller do
 
-# #   # describe 'GET #new' do
-# #   #   it 'assigns a new user to @user' do
-# #   #     get :new
-# #   #     expect(assigns(:user)).to be_a_new(User)
-# #   #   end
-
-# #   #   it 'renders the new template' do
-# #   #     get :new
-# #   #     expect(response).to render_template :new
-# #   #   end
-# #   # end
-
-# #   describe '#signup' do
-# #     context 'with valid attributes' do
-# #       let(:user) {FactoryGirl.build :user}
-# #       it 'saves the new user to the database' do
-# #         expect {post :create, :user => @user}.to change(User, :count).by(1)
-# #       end
-# #     end
-
-# #     context 'with invalid attributes' do
-# #       let(:user) {FactoryGirl.build :invalid_user}
-# #       it 'does not save the new user to the database' do
-# #         expect {post :create, :user => @user}.to change(User, :count).by(0)
-# #       end
-# #     end
-# #   end
-
-# #   describe '#login' do
-# #     context 'with valid attributes' do
-# #       let(:user) {FactoryGirl.build :user}
-# #       it 'logs in a valid user' do
-# #         post :login
-# #         expect(assigns(:user)).to eq user
-# #       end
-# #     end
-
-# #     context 'with invalid attributes' do
-# #       let(:user) {FactoryGirl.build :invalid_user}
-# #       it 'does not log in an invalid user' do
-# #         expect(assigns(:invalid_user)).to not_eq user
-# #       end
-# #     end
-
-# #   end
-
-# end
+  describe 'GET registrations#edit' do
+    it "renders the user's edit page" do
+      expect(response).to render_template(:new)
+    end
+  end
 
 
+  describe 'PATCH registrations#edit' do
+    before :each do
+      @user = create(:user)
+    end
+
+    # let(:user) {FactoryGirl.build :user}
+    context 'valid attributes' do
+      it "locates the user" do
+        patch :update, id: @user, user: attributes_for(:user)
+        expect(assigns(:user)).to eq(@user)
+      end
+
+      it "changes user's attributes" do
+        patch :update, id: @user,
+          user: attributes_for(:user, name: "Bob")
+          @user.reload
+          expect(@user.name).to eq("Bob")
+      end
+    end
+  end
+end
